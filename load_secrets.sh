fc943fca4212300171bc8b695c3374f2b5013112
mrcbns <moracabanas@live.com>
2021-01-07 10:48:49 +0100

    Create load_secrets.sh
    It load secrets from  XYZ_VARIABLE_FILE which allows to use docker secrets properly. (blatantly stolen from mysql 5.7 official Dockerfile Entrypoint.)

---
+++ load_secrets.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
+
+log() {
+	local type="$1"; shift
+	printf '%s [%s] [CMD]: %s\n' "$(date --rfc-3339=seconds)" "$type" "$*"
+  
+error() {
+	log ERROR "$@" >&2
+	exit 1
+}
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_REGISTRY_PASS' 'example'
+# (will allow for "$XYZ_REGISTRY_PASS_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		mysql_error "Both $var and $fileVar are set (but are exclusive)"
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# Initialize values that might be stored in a file
+	file_env 'REGISTRY_HOST' '%'
+	file_env 'REGISTRY_SSL'
+	file_env 'REGISTRY_DOMAIN'
+	file_env 'REGISTRY_STORAGE_DELETE_ENABLED'
+	file_env 'REGISTRY_USER'
+  file_env 'REGISTRY_PASS'
